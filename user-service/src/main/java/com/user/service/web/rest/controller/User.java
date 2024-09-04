34e153c242914b81195ccafe41eac652b805140e
Manoj Chowdary <manojchow72@gmail.com>
2024-09-04 21:18:04 +0530

    Added User Service

--- user-service/src/main/java/com/user/service/web/rest/controller/User.java
+++ user-service/src/main/java/com/user/service/web/rest/controller/User.java
@@ -1,5 +1,7 @@
 package com.user.service.web.rest.controller;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +11,8 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class User {
 
     private String firstName;
